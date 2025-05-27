37a25bd099b71235162092a15134823d99b2daa0
Mauricio Samson <450322@student.fontys.nl>
2025-05-27 14:07:30 +0200

    fix connectionstring

--- DAL/TrendContextFactory.cs
+++ DAL/TrendContextFactory.cs
@@ -10,7 +10,7 @@ namespace DAL
             var optionsBuilder = new DbContextOptionsBuilder<TrendContext>();
 
             // Replace this with your actual local or dev connection string
-            optionsBuilder.UseNpgsql("Host=dpg-d0qnu0jipnbc73eic3i0-a;Port=5432;Database=trenddatabase;Username=dbi525146_trends;Password=G7EAbSmFlZ3n7tTpw0oT8hWSSWAsykRq;SSL Mode=Require;Trust Server Certificate=true");
+            optionsBuilder.UseNpgsql("Host=dpg-d0qnu0jipnbc73eic3i0-a;Port=5432;Database=trenddatabase;Username=dbi525146_trends;Password=G7EAbSmFlZ3n7tTpw0oT8hWSSWAsykRq;SSL Mode=Require;Trust Server Certificate=true;");
 
             return new TrendContext(optionsBuilder.Options);
         }
